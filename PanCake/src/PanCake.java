083ffb9af05f5e333c15e0e0ae65de1b891b54f6
enihsyou <s1131234@gmail.com>
2016-11-07 12:41:52 +0800

    改一下，按老师要求来写

--- PanCake/src/PanCake.java
+++ PanCake/src/PanCake.java
@@ -1,17 +1,17 @@
+import java.util.Date;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
 
 class Pan {
     private final Random random;
-    private AtomicInteger totalSold; //当前销售量
-    private AtomicInteger currentForSell;//当前库存量
+    private int totalSold; //当前销售量
+    private int currentForSell;//当前库存量
     private static final int MAX_CAPACITY = 3;//库存量
     private static final int MAX_TODAY_SOLD = 20; //最大销售量
     private boolean soldOut;
 
     Pan() {
-        totalSold = new AtomicInteger(0);
-        currentForSell = new AtomicInteger(1);
+        totalSold = 0;
+        currentForSell = 1;
         soldOut = false;
         random = new Random();
 
@@ -19,25 +19,25 @@ class Pan {
     }
 
     void makeOne() throws InterruptedException {
-        int waitTime = random.nextInt(1000) + 1000;
-        Thread.sleep(waitTime);
-        if (totalSold.get() >= MAX_TODAY_SOLD) return;
-        if (currentForSell.get() < MAX_CAPACITY) {
-            System.out.printf("过了%ds %s制作了一个，现在有%d个蛋饼了\n", waitTime / 1000, Thread.currentThread().getName(),
-                    currentForSell.incrementAndGet());
-        } else {
-            System.out.printf("等了%ds，没人来买···\n", waitTime / 1000);
+        Thread.sleep(random.nextInt(3000) + 1000);
+        synchronized (this) {
+            if (totalSold >= MAX_TODAY_SOLD) return;
+            if (currentForSell < MAX_CAPACITY) {
+                System.out.printf("%tH:%<tM:%<tS %s制作了一个，现在有%d个蛋饼了\n", new Date(), Thread.currentThread().getName(),
+                        ++currentForSell);
+            } else System.out.printf("%tH:%<tm:%<tS 等啊等，没人来买···\n", new Date());
         }
     }
 
     void soldOne() throws InterruptedException {
-        int waitTime = random.nextInt(3000) + 1000;
-        Thread.sleep(waitTime);
-        if (currentForSell.get() > 0) {
-            System.out.printf("过了%ds %s来买了一个，盘子里剩下%d个，已经卖出%d个\n", waitTime / 1000, Thread.currentThread().getName(),
-                    currentForSell.decrementAndGet(), totalSold.incrementAndGet());
-        } else System.out.printf("%s白等了%ds，饿着离开了\n", Thread.currentThread().getName(), waitTime / 1000);
-        if (totalSold.get() >= MAX_TODAY_SOLD) soldOut = true;
+        Thread.sleep(random.nextInt(3000) + 1000);
+        synchronized (this) {
+            if (currentForSell > 0) {
+                System.out.printf("%tH:%<tM:%<tS %s来买了一个，盘子里剩下%d个，已经卖出%d个\n", new Date(), Thread.currentThread().getName(),
+                        --currentForSell, ++totalSold);
+            } else System.out.printf("%tH:%<tm:%<tS %s来了没买到，饿着离开了\n", new Date(), Thread.currentThread().getName());
+            if (totalSold >= MAX_TODAY_SOLD) soldOut = true;
+        }
     }
 
 
@@ -48,12 +48,10 @@ class Pan {
 
 class Maker implements Runnable {
     private Pan pan;
-    Thread thread;
 
     Maker(Pan pan) {
         this.pan = pan;
-
-        thread = new Thread(this, "卖蛋饼的");
+        new Thread(this, "卖蛋饼的").start();
     }
 
     @Override
@@ -71,12 +69,10 @@ class Maker implements Runnable {
 
 class BaseBuyer implements Runnable {
     private Pan pan;
-    Thread thread;
 
     BaseBuyer(Pan pan) {
         this.pan = pan;
-
-        thread = new Thread(this, "买蛋饼的");
+        new Thread(this, "买蛋饼的").start();
     }
 
     @Override
@@ -94,14 +90,7 @@ class BaseBuyer implements Runnable {
 public class PanCake {
     public static void main(String[] args) {
         Pan pan = new Pan();
-        Maker maker = new Maker(pan);
-        BaseBuyer buyer = new BaseBuyer(pan);
-        maker.thread.start();
-        buyer.thread.start();
-        try {
-            maker.thread.join();
-            buyer.thread.join();
-        } catch (InterruptedException ignore) {
-        }
+        new Maker(pan);
+        new BaseBuyer(pan);
     }
 }
