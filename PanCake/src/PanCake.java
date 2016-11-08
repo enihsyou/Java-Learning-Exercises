4fe921167e3c4857ee68e5c6daedfce37d3b3b1b
enihsyou <s1131234@gmail.com>
2016-11-08 23:21:09 +0800

    格式化一下

--- PanCake/src/PanCake.java
+++ PanCake/src/PanCake.java
@@ -5,16 +5,16 @@ import java.util.concurrent.atomic.AtomicBoolean;
 class Pan {
     private final Random random;
     private static final int MAX_CAPACITY = 3;//库存量
-    private static final int MAX_TODAY_SOLD = 20; //最大销售量
+    private static final int MAX_TODAY_SOLD = 20;//最大销售量
     private int totalSold; //当前销售量
     private int currentForSell;//当前库存量
     private AtomicBoolean soldOut; //flag
 
     Pan() {
+        random = new Random();
         totalSold = 0;
         currentForSell = 1;
         soldOut = new AtomicBoolean(false);
-        random = new Random();
 
         System.out.println("盘古开天辟地留下了一个蛋饼");
     }
@@ -22,7 +22,7 @@ class Pan {
     void makeOne() throws InterruptedException {
         Thread.sleep(random.nextInt(5000) + 1000);
         synchronized (this) {
-            if (totalSold + currentForSell >= MAX_TODAY_SOLD) return;
+            if (totalSold + currentForSell >= MAX_TODAY_SOLD) return; //做够了，退出
             if (currentForSell < MAX_CAPACITY) {
                 System.out.printf("%tH:%<tM:%<tS %s制作了一个，现在有%d个蛋饼了\n", new Date(), Thread.currentThread().getName(),
                         ++currentForSell);
@@ -34,13 +34,12 @@ class Pan {
         Thread.sleep(random.nextInt(5000) + 1000);
         synchronized (this) {
             if (currentForSell > 0) {
-                System.out.printf("%tH:%<tM:%<tS %s来买了一个，盘子里剩下%d个，已经卖出%d个\n", new Date(),
-                        Thread.currentThread().getName(), --currentForSell, ++totalSold);
+                System.out.printf("%tH:%<tM:%<tS %s来买了一个，盘子里剩下%d个，已经卖出%d个\n", new Date(), Thread.currentThread()
+                        .getName(), --currentForSell, ++totalSold);
             } else System.out.printf("%tH:%<tm:%<tS %s来了没买到，饿着离开了\n", new Date(), Thread.currentThread().getName());
             if (totalSold >= MAX_TODAY_SOLD) soldOut.set(true);
         }
     }
-
 
     boolean isSoldOut() {
         return soldOut.get();
@@ -60,9 +59,7 @@ class Maker implements Runnable {
         try {
             while (!pan.isSoldOut()) pan.makeOne();
             System.out.println("卖完收工！");
-        } catch (InterruptedException ignore) {
-            // System.out.println("城管来了！不让卖了！");
-        }
+        } catch (InterruptedException ignore) {}
     }
 }
 
@@ -78,9 +75,7 @@ class BaseBuyer implements Runnable {
     public void run() {
         try {
             while (!pan.isSoldOut()) pan.soldOne();
-        } catch (InterruptedException ignore) {
-            // System.out.println("不让买了！");
-        }
+        } catch (InterruptedException ignore) {}
     }
 }
 
