ee4a3534f7d7081814fb6f0c81986567b7f2875b
enihsyou <s1131234@gmail.com>
2016-11-13 01:06:47 +0800

    Criminal Intent第一部分

---
+++ CriminalIntent/app/src/main/java/com/enihsyou/shane/criminalintent/Crime.java
@@ -0,0 +1,24 @@
+package com.enihsyou.shane.criminalintent;
+
+import java.util.UUID;
+
+public class Crime {
+    private UUID mID;
+    private String mTitle;
+
+    public Crime() {
+        mID = UUID.randomUUID();
+    }
+
+    public UUID getID() {
+        return mID;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
